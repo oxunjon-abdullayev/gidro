fc8792a81f7aa425ab8b0d239a039985521cac2d
oxunjon <abdullayevoxunjon757@gmail.com>
2023-06-16 17:48:35 +0500

    my file

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -8,9 +8,22 @@ from loader import dp
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
 
-    if message.from_user.id == 1295742159 :
+    if message.from_user.id == 1295742159:
         await message.answer(text=f"Admin xush kelibsiz !",
                              reply_markup=user_rkm())
     else:
         await message.answer(f"Salom, {message.from_user.full_name}!",
                              reply_markup=user_panel_default_button())
+
+
+
+#
+# @dp.message_handler(CommandStart(),IsAdmin())
+# async def bot_start(message: types.Message):
+#     await message.answer(text=f"Admin xush kelibsiz !",
+#                              reply_markup=user_rkm())
+
+# @dp.message_handler(CommandStart())
+# async def bot_user_start(message:types.Message):
+#         await message.answer(f"Salom, {message.from_user.full_name}!",
+#                              reply_markup=user_panel_default_button())
