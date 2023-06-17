375fdff0329b270134eb7866ddd5d62bcbe7e292
oxunjon <abdullayevoxunjon757@gmail.com>
2023-06-17 12:09:04 +0500

    git conf o'zgarish

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -18,11 +18,16 @@ async def bot_start(message: types.Message):
 
 
 
+
+
+
+
+
 # @dp.message_handler(CommandStart(),IsAdmin())
 # async def bot_start(message: types.Message):
 #     await message.answer(text=f"Admin xush kelibsiz !",
 #                              reply_markup=user_rkm())
-
+#
 # @dp.message_handler(CommandStart())
 # async def bot_user_start(message:types.Message):
 #         await message.answer(f"Salom, {message.from_user.full_name}!",
